a64e3ab80d4f2753770af5e189b651533b46821d
blue-ayye <ccoolbuddy4@gmail.com>
2022-08-16 00:12:56 +0530

    Domain Model - created AppUser and Role classes

---
+++ src/main/java/com/blazingpotato/securityapp/domain/AppUser.java
@@ -0,0 +1,27 @@
+package com.blazingpotato.securityapp.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static javax.persistence.FetchType.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    @ManyToMany(fetch = EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+}
